1b26aeab578cc98a68f924b1026a3d5c0bee867b
rqphy <raphael.ferreira@hetic.net>
2024-03-18 11:07:07 +0100

    :sparkles::lipstick: update secondary cards style and handle buttons click events

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -5,13 +5,26 @@ interface CardProps {
 }
 
 export default function Card({ data }: CardProps) {
+    const handleCallBack = (_event: React.MouseEvent) => {
+        // window.location.href = _event.dataset.url
+        console.log("location changed")
+    }
+
     return (
-        <li className={`card card--${data.type}`}>
+        <li
+            className={`card card--${data.type}`}
+            data-url="#"
+            onClick={handleCallBack}
+        >
             <p className="card__title">{data.title}</p>
             <figure className="card__illustration">
                 <img src={data.illustrationSrc} alt={data.title} />
             </figure>
-            <button className="card__button">
+            <button
+                className="card__button"
+                data-url="#"
+                onClick={handleCallBack}
+            >
                 {data.buttonLabel}
                 {data.type === "primary" && (
                     <figure>
